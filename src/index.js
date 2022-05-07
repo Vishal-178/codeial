70b4099d3ae81aac776439fa511f4e672a440451
vishal singh <vishalkumarmps@gmail.com>
2022-05-07 11:14:43 +0530

    setup and worked on css and some pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+import Navbar from './components/Navbar';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import Comment from '../components/Comment';
+import styles from '../styles/home.module.css';
+
+const Home = ({ posts }) => {
+  return (
+    <div className={styles.postsList}>
+      {posts.map((post) => (
+        <div className={styles.postWrapper} key={`post-${post._id}`}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
+                alt="user-pic"
+              />
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
+            </div>
+            <div className={styles.postContent}>{post.conent}</div>
+
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                  alt="likes-icon"
+                />
+                <span>5</span>
+              </div>
+
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1380/1380338.png"
+                  alt="comments-icon"
+                />
+                <span>2</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              <Comment comment={post} />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Home.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default Home;
